aa22dbef7559a8325406e7f94edeee93fd4e1e59
Mike Pesate <>
2018-09-19 16:18:52 +0200

    Templates updated to fit new formats

--- VIPER Templates/Interactor Worker Template.xctemplate/___FILEBASENAME___Interactor.swift
+++ VIPER Templates/Interactor Worker Template.xctemplate/___FILEBASENAME___Interactor.swift
@@ -9,20 +9,24 @@
 import Foundation
 
 protocol ___VARIABLE_productName:identifier___InteractorProtocol: BaseInteractorProtocol {
-    
+
 }
 
 protocol ___VARIABLE_productName:identifier___InteractorCallbackProtocol: BaseInteractorCallbackProtocol {
-    
+
 }
 
 class ___VARIABLE_productName:identifier___Interactor: BaseInteractor {
-    
+
     weak var presenter: ___VARIABLE_productName:identifier___InteractorCallbackProtocol!
-    
+    fileprivate let worker: ___VARIABLE_productName:identifier___WorkerAlias
+
+    init(with___VARIABLE_productName:identifier___Worker worker: ___VARIABLE_productName:identifier___WorkerAlias = ___VARIABLE_productName:identifier___Worker()) {
+        self.worker = worker
+        super.init()
+    }
 }
 
 extension ___VARIABLE_productName:identifier___Interactor: ___VARIABLE_productName:identifier___InteractorProtocol {
-    
-    
+
 }
